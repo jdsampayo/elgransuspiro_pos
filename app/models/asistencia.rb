c0cfee7e857ea4535e1462fd8ad8760e6fcdc646
Jorge Daniel Sampayo Vargas <jorge@regalii.com>
2018-04-18 18:00:56 -0500

    Fix count of asistencias

--- app/models/asistencia.rb
+++ app/models/asistencia.rb
@@ -2,7 +2,12 @@ class Asistencia < ApplicationRecord
   belongs_to :mesero
   belongs_to :corte
 
+  before_create :checar_retardo
   before_update :calcular_horas
+
+  scope :activas, -> {
+    where.not(horas: nil)
+  }
 
   def calcular_horas
     self.horas = ((updated_at - created_at) / 1.hour).to_i
@@ -10,6 +15,10 @@ class Asistencia < ApplicationRecord
 
   def preview_de_salida
     ((Time.current - created_at) / 1.hour).to_i
+  end
+
+  def checar_retardo
+    self.retardo = true if Time.current.min >= 10
   end
 
   def self.meseros_del_dia
