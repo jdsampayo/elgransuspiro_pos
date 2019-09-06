6846d88be529bd2aaef17018fd08a13aafdf2d73
Jorge Daniel Sampayo Vargas <jdsampayo@gmail.com>
2019-09-06 16:46:16 -0500

    Remove deprecated AuthLogic validation

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -12,15 +12,50 @@
 #
 
 class Usuario < ActiveRecord::Base
+  EMAIL = /
+    \A
+    [A-Z0-9_.&%+\-']+   # mailbox
+    @
+    (?:[A-Z0-9\-]+\.)+  # subdomains
+    (?:[A-Z]{2,25})     # TLD
+    \z
+  /ix
+
   acts_as_authentic do |configuration|
     configuration.session_class = Sesion
-    configuration.validates_format_of_email_field_options = {with: Authlogic::Regex.email_nonascii}
   end
 
   validates_presence_of :email, :password, :password_confirmation
 
+  validates :email,
+    format: {
+      with: EMAIL,
+      message: proc {
+        ::Authlogic::I18n.t(
+          "error_messages.email_invalid",
+          default: "should look like an email address."
+        )
+      }
+    },
+    length: { maximum: 100 },
+    uniqueness: {
+      case_sensitive: false,
+      if: :will_save_change_to_email?
+    }
+
+  validates :password,
+    confirmation: { if: :require_password? },
+    length: {
+      minimum: 8,
+      if: :require_password?
+    }
+  validates :password_confirmation,
+    length: {
+      minimum: 8,
+      if: :require_password?
+  }
+
   def to_s
     email
   end
-
 end
