f6217bef3242f14efac30c5c96a70b04ea12b182
Jorge Daniel Sampayo Vargas <jdsampayo@gmail.com>
2019-12-03 19:04:50 -0600

    Fix cortes show

--- app/controllers/cortes_controller.rb
+++ app/controllers/cortes_controller.rb
@@ -55,56 +55,13 @@ class CortesController < ApplicationController
   end
 
   def propinas
+    @propinas = Corte.order(dia: :desc).page(params[:page]).pluck(:dia, :propinas)
     @cortes = Corte.includes(:asistencias).order(dia: :desc).page params[:page]
-
-    @semana_actual = [
-      {
-        name: "Propinas",
-        data: Corte.de_la_semana(Date.today, :propinas)
-      }
-    ]
-    @semana_anterior = [
-      {
-        name: "Propinas",
-        data: Corte.de_la_semana(1.week.ago, :propinas)
-      }
-    ]
-
-    por_semana_query = Corte.unscoped.group_by_week(:dia, time_zone: false)
-    @por_semana = [
-      {
-        name: "Propinas",
-        data: por_semana_query.sum(:propinas)
-      }
-    ]
   end
 
   # GET /cortes/1
   def show
-    @comandas = @corte.comandas
-    @gastos = Gasto.where(corte_id: @corte.id)
-
-    @con_tarjeta = @comandas.con_tarjeta
-    @con_efectivo = @comandas.con_efectivo
-
-    @total_comandas_cerradas = @comandas.cerradas.sum(:total)
-    @total_con_tarjeta = @con_tarjeta.cerradas.sum(:total)
-
-    @total_de_gastos = @corte.sum_gastos
-
-    @caja = @corte.inicial + @total_comandas_cerradas - @total_con_tarjeta - @total_de_gastos
-
-    @propinas = @comandas.sum(:propina)
-    @total_de_ventas = @comandas.sum(:total)
-
-    @estancia_promedia = @comandas.map do |comanda|
-      comanda.closed_at - comanda.created_at if comanda.closed_at
-    end.compact
-    @estancia_promedia = @estancia_promedia.sum / @estancia_promedia.count unless @estancia_promedia.empty?
-
-    @total_de_productos = Orden.where(comanda_id: @comandas).sum(:cantidad)
-
-    @cheque_promedio = @total_de_ventas / @comandas.sum(:comensales) unless @comandas.empty?
+    @corte.set_subtotals
   end
 
   # GET /cortes/new
