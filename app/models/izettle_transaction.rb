78fce5348fb20dea0c33ef22df3d19aee28d2d96
Jorge Daniel Sampayo Vargas <jdsampayo@gmail.com>
2020-11-30 23:16:53 -0600

    Add new izettle transaction type

--- app/models/izettle_transaction.rb
+++ app/models/izettle_transaction.rb
@@ -5,7 +5,7 @@ require 'integrations/izettle'
 class IzettleTransaction < ApplicationRecord
   belongs_to :izettle_purchase, optional: true
 
-  enum transaction_type: %i[payout card_payment card_payment_fee card_refund card_payment_fee_refund failed_payout payment_fee]
+  enum transaction_type: %i[payout card_payment card_payment_fee card_refund card_payment_fee_refund failed_payout payment_fee payment]
 
   validates_uniqueness_of :transacted_at, scope: [:transaction_type, :amount]
 
@@ -16,7 +16,8 @@ class IzettleTransaction < ApplicationRecord
     card_refund: 'Reembolso de pago',
     card_payment_fee_refund: 'Reembolso de comisión',
     failed_payout: 'Pago fallido',
-    payment_fee: 'Comisión de pago'
+    payment_fee: 'Comisión de pago',
+    payment: 'Pago'
   }.freeze
 
   scope :del_dia, ->(dia) {
